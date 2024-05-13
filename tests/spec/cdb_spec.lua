eb0c0abada16cf7fafd6dc81d210140c64ebbc18
Kafva <36083692+Kafva@users.noreply.github.com>
2024-05-13 06:08:08 +0200

    check that sign is placed

--- tests/spec/cdb_spec.lua
+++ tests/spec/cdb_spec.lua
@@ -15,6 +15,15 @@ local function rm_f(filepath)
     end
 end
 
+---@param group string
+---@param lnum number
+---@return boolean
+local function sign_exists(group, lnum)
+    local buf = vim.api.nvim_get_current_buf()
+    local bufsigns = vim.fn.sign_getplaced(buf, { group = group, lnum = tostring(lnum) })
+    return #bufsigns > 0 and #bufsigns[1].signs > 0
+end
+
 --- Tests are not ran in parallel
 describe("lldb/gdb breakpoints:", function()
     before_each(function()
@@ -33,12 +42,14 @@ describe("lldb/gdb breakpoints:", function()
 
         local content = util.readfile('.lldbinit')
         assert.equals("breakpoint set --file ../files/main.c --line 9\n", content)
+        assert(sign_exists('brk', 9), 'no sign placed at line 9')
 
         -- Remove breakpoint
         cdb.toggle_breakpoint('lldb', './.lldbinit', nil, 9)
 
         content = util.readfile('.lldbinit')
         assert.equals("", content)
+        assert(not sign_exists('brk', 9), 'sign still placed at line 9')
     end)
 
     it("Toggle a breakpoint in gdbinit", function()
@@ -47,11 +58,13 @@ describe("lldb/gdb breakpoints:", function()
 
         local content = util.readfile('.gdbinit')
         assert.equals("break ../files/main.c:7\n", content)
+        assert(sign_exists('brk', 7), 'no sign placed at line 7')
 
         -- Remove breakpoint
         cdb.toggle_breakpoint('gdb', './.gdbinit', nil, 7)
 
         content = util.readfile('.gdbinit')
         assert.equals("", content)
+        assert(not sign_exists('brk', 7), 'sign still placed at line 7')
     end)
 end)
